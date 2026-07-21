08f6f0d173839bcf4567add7d38d6428132197aa
Shivanisunil281109 <shivaniesonawane@gmail.com>
2026-07-21 11:59:14 +0530

    updates

--- react-app/src/pages/Products.jsx
+++ react-app/src/pages/Products.jsx
@@ -13,22 +13,22 @@ function Products(){
     {
       title: "Shirt",
       price: 450,
-      image: "https://m.media-amazon.com/images/I/71SxJ4j0W-L._AC_UL480_FMwebp_QL65_.jpg"
+      image: "https://m.media-amazon.com/images/I/51A--HMRXRL._AC_UL480_FMwebp_QL65_.jpg"
     },
     {
       title: "Jeans",
       price: 799,
-      image: "https://m.media-amazon.com/images/I/71YJQv5QwNL._AC_UL480_FMwebp_QL65_.jpg"
+      image: "https://m.media-amazon.com/images/I/71DjSVa+7rL._AC_UL480_FMwebp_QL65_.jpg"
     },
     {
       title: "Hoodie",
       price: 999,
-      image: "https://m.media-amazon.com/images/I/71RjR9lQ9TL._AC_UL480_FMwebp_QL65_.jpg"
+      image: "https://m.media-amazon.com/images/I/81bDC5U8IIL._AC_UL480_FMwebp_QL65_.jpg"
     },
     {
       title: "Jacket",
       price: 1499,
-      image: "https://m.media-amazon.com/images/I/81M8Q9v6vPL._AC_UL480_FMwebp_QL65_.jpg"
+      image: "https://m.media-amazon.com/images/I/61apVzGxlnL._AC_UL480_FMwebp_QL65_.jpg"
     },
     {
       title: "Sneakers",
@@ -38,25 +38,40 @@ function Products(){
     {
       title: "Watch",
       price: 2499,
-      image: "https://m.media-amazon.com/images/I/71w3oJ7aWyL._AC_UL480_FMwebp_QL65_.jpg"
+      image: "https://m.media-amazon.com/images/I/81nj6IlZpVL._AC_UL480_FMwebp_QL65_.jpg"
     },
     {
       title: "Cap",
       price: 299,
-      image: "https://m.media-amazon.com/images/I/71q8Q5Q2WCL._AC_UL480_FMwebp_QL65_.jpg"
+      image: "https://m.media-amazon.com/images/I/51QVfsGxLFL._AC_UL480_FMwebp_QL65_.jpg"
     },
     {
       title: "Backpack",
       price: 1299,
-      image: "https://m.media-amazon.com/images/I/81Ke4QhYfDL._AC_UL480_FMwebp_QL65_.jpg"
+      image: "https://m.media-amazon.com/images/I/61SWPUP9iDL._AC_UL480_FMwebp_QL65_.jpg"
     },
     {
-      title: "Sunglasses",
-      price: 599,
-      image: "https://m.media-amazon.com/images/I/61K8wH8mQTL._AC_UL480_FMwebp_QL65_.jpg"
+      title: "Parfum",
+      price: 299,
+      image: " https://m.media-amazon.com/images/I/4166284dp5L._AC_SR250,250_QL65_.jpg"
+      
     }
   ];
 
+async function getProducts(){
+try {
+  const response=await axios.get("/products/all");
+if(response.data.success){
+
+}
+
+
+} catch (error) {
+  console.log(error);
+}
+
+}
+getProducts();
 
 
 
@@ -66,14 +81,19 @@ function Products(){
 <h1> Products</h1>
 
 
-{productData.map((product)=>(<h1>{product.title} </h1>))}
 
 
 <div className="productcardstyle" >  
+
+{productData.map((product)=>(
+
 <ProductCard 
-image="https://m.media-amazon.com/images/I/41O6-frKRLL._AC_UL480_FMwebp_QL65_.jpg" 
-title="Tshirt" 
-price="200"    />
+image={product.image}
+title={product.title}
+price={product.price}   
+/>
+))}
+
 
 
 {/* 
